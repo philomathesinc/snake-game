b54db7095c7e287574556251fe1728a29f98b13d
Suraj Kamath <suraj.kamath.aux@flip.id>
2023-02-11 10:55:29 +0530

    fyne Hello World setup
    Signed-off-by: Suraj Kamath <suraj.kamath.aux@flip.id>

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }

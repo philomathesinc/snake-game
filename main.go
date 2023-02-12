1eddf149dc1f99765e55d7fdd8779c3063c96a51
Mriyam Tamuli <mbtamuli@gmail.com>
2023-02-12 11:22:24 +0530

    WIP: move snake
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- main.go
+++ main.go
@@ -9,30 +9,56 @@ import (
 	"fyne.io/fyne/v2/container"
 )
 
+const (
+	finalSpaceWidth  = 840
+	finalSpaceHeight = 840
+	singlePix        = 40
+)
+
+type game struct {
+	window fyne.Window
+	snake  canvas.Rectangle
+}
+
+var (
+	green        = color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	gameInstance = game{}
+)
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
 	w.Resize(fyne.Size{
-		Width:  800,
-		Height: 800,
+		Width:  finalSpaceWidth,
+		Height: finalSpaceHeight,
 	})
 
 	w.CenterOnScreen()
 
-	gameWindow := getGameWindow(20, 20)
+	gameInstance.snake = *canvas.NewRectangle(green)
+	gameInstance.snake.Resize(fyne.NewSize(singlePix, singlePix))
 
-	w.SetPadded(false)
-	w.SetContent(gameWindow)
+	gameInstance.snake.Move(fyne.NewPos((finalSpaceWidth-singlePix)/2, (finalSpaceHeight-singlePix)/2))
+
+	content := container.NewWithoutLayout(&gameInstance.snake)
+	w.SetContent(content)
+	w.Canvas().SetOnTypedKey(printKeys)
+
+	gameInstance.window = w
+
 	w.ShowAndRun()
 }
 
-func getGameWindow(rows, cols int) fyne.CanvasObject {
-	var rectangles []fyne.CanvasObject
-	for i := 0; i < rows*cols; i++ {
-		rect := canvas.NewRectangle(color.White)
-		rectangles = append(rectangles, rect)
+func printKeys(ev *fyne.KeyEvent) {
+	if ev.Name == fyne.KeyW {
+		gameInstance.snake.Position().AddXY(0, singlePix)
+	} else if ev.Name == fyne.KeyS {
+		gameInstance.snake.Position().AddXY(0, -singlePix)
+	} else if ev.Name == fyne.KeyA {
+		gameInstance.snake.Position().AddXY(-singlePix, 0)
+	} else if ev.Name == fyne.KeyD {
+		gameInstance.snake.Position().AddXY(singlePix, 0)
 	}
 
-	return container.NewAdaptiveGrid(20, rectangles...)
-
+	gameInstance.window.Canvas().Refresh(&gameInstance.snake)
 }

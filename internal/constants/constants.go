b07c48d13515eee062c721c571f8cd37cb415333
Mugdha Adhav <mugdha.adhav@gmail.com>
2023-03-18 11:39:29 +0530

    Create package pellet, score_counter and window

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,18 +2,8 @@ package constants
 
 import (
 	"image/color"
-
-	"fyne.io/fyne/v2"
-)
-
-const (
-	FinalSpaceWidth  = 840
-	FinalSpaceHeight = 840
-	SinglePix        = 40
-	PixelCountLimit  = 21
 )
 
 var (
-	CenterGamePixel = fyne.NewPos((FinalSpaceWidth-SinglePix)/2, (FinalSpaceHeight-SinglePix)/2)
-	Green           = color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	Green = color.NRGBA{R: 0, G: 180, B: 0, A: 255}
 )
